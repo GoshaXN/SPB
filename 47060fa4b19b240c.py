b5f783728b5c56b9fe2da70c9e8d07398f0d9288
GoshaXN <91261919+GoshaXN@users.noreply.github.com>
2021-09-23 15:00:30 +0300

    Rename 47060fa4b19b240c.py to HACK(10)

--- 47060fa4b19b240c.py
+++
@@ -1,6 +0,0 @@
-import time
-import random
-from rich import print
-while True:
-	time.sleep(0.07)
-	print(random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1))
